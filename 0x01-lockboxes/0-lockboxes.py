aaed6234ffe93067ba02d59af31ef49f1f81ed5f
GiddyLesGid <gideonlangat@gmail.com>
2023-08-01 11:43:38 +0000

    locked boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-'''canUnlockAll Function Documentation'''
+'''LockBoxes Challenge'''
+
 
 def canUnlockAll(boxes):
-    '''Determines if all the boxes can be opened or not
+    '''determines if all the boxes can be opened or not
     Returns:
         True: all boxes can be opened
         False: not all boxes can be opened
@@ -13,20 +14,19 @@ def canUnlockAll(boxes):
     i = 0
 
     while i < length:
-        previous_index = i
+        oldi = i
         opened_boxes.append(i)
         keys.update(boxes[i])
         for key in keys:
             if key != 0 and key < length and key not in opened_boxes:
                 i = key
                 break
-            if previous_index != i:
-                continue
-            else:
-                break
-        
-        for i in range(length):
-            if i not in opened_boxes and i != 0:
-                return False
-        return True
+        if oldi != i:
+            continue
+        else:
+            break
 
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
