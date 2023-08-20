16e83dfaa14e5ea7d40de3a4038a1bdf2557d352
GiddyLesGid <gideonlangat@gmail.com>
2023-08-20 05:52:48 -0700

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,59 +1,36 @@
 #!/usr/bin/python3
 
-'''A script that reads stdin line by line and computes metrics.'''
+'''A script that reads stdin line by line and computes metrics'''
 
 import sys
 
-# Initialize a dictionary to store status code counts
-status_counts = {'200': 0, '301': 0, '400': 0, '401': 0,
-                 '403': 0, '404': 0, '405': 0, '500': 0}
-
-# Initialize variables for total file size and line counter
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-line_count = 0
+counter = 0
 
 try:
-    # Read each line from stdin
     for line in sys.stdin:
-        line = line.strip()
-        parts = line.split()
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-        # Check if the line format is valid
-        if len(parts) == 9:
-            status_code = parts[7]
-            try:
-                file_size = int(parts[8])
-
-                # Update status code count if applicable
-                if status_code in status_counts:
-                    status_counts[status_code] += 1
-
-                # Update total file size and line counter
-                total_size += file_size
-                line_count += 1
-
-                # Print statistics every 10 lines
-                if line_count % 10 == 0:
-                    print('File size:', total_size)
-                    for code, count in sorted(status_counts.items()):
-                        if count != 0:
-                            print(f'{code}: {count}')
-                    print()
-
-            except KeyboardInterrupt:
-                # Print statistics and exit on keyboard interruption
-                print('File size:', total_size)
-                for code, count in sorted(status_counts.items()):
-                    if count != 0:
-                        print(f'{code}: {count}')
-                sys.exit(0)
-            except Exception:
-                pass
+        if counter == 10:
+            counter = 0
+            print('File size:', total_size)
+            for key in sorted(cache, key=int):
+                if cache[key] != 0:
+                    print(f'{key}: {cache[key]}')
 
 except KeyboardInterrupt:
-    # Print statistics and exit on keyboard interruption
+    pass
+finally:
     print('File size:', total_size)
-    for code, count in sorted(status_counts.items()):
-        if count != 0:
-            print(f'{code}: {count}')
-    sys.exit(0)
+    for key in sorted(cache, key=int):
+        if cache[key] != 0:
+            print(f'{key}: {cache[key]}')
